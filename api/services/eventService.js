b719202e349343d90f048b8ba68b3d0ae97b6f97
basukinath thakur <basukinaththakur15082006@gmail.com>
2025-09-24 17:43:02 +0530

    Update API_URL to point to the correct server endpoint

--- api/services/eventService.js
+++ api/services/eventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getToken } from './authService';
 
-const API_URL = 'https://kaali-mandir.vercel.app/api';
+const API_URL = 'https://kaali-mandir.onrender.com/api';
 
 // Configure axios with auth token
 const authAxios = () => {
